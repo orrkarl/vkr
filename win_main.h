d2e013db83c72631dc0a2939a1e198f69e4ab44e
ububuntu <karlor041@gmail.com>
2018-11-14 15:45:26 +0200

    clean slate; restarting as n-dim rasterizer

--- win_main.h
+++
@@ -1,9 +0,0 @@
-#ifndef CORE_WIN_MAIN_H_
-#define CORE_WIN_MAIN_H_
-
-#include <Windows.h>
-
-int main();
-int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int mCmdShow);
-
-#endif // CORE_WIN_MAIN_H_
