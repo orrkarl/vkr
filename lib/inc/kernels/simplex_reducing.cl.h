40dc2905a691397fb8e40020abe8c2035838aa01
orrkarl <karlor041@gmail.com>
2019-07-13 13:38:18 +0300

    added simplex reducing

---
+++ lib/inc/kernels/simplex_reducing.cl.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include "../general/predefs.h"
+
+namespace nr
+{
+
+namespace __internal
+{
+
+namespace clcode
+{
+
+// All of the cross-file utilities and types used in my cl code are here
+const string vertex_reducing = R"__CODE__(
+#define TRIANGLES_PER_SIMPLEX (RENDER_DIMENSION * (RENDER_DIMENSION - 1) * (RENDER_DIMENSION - 2) / 6) // There are nC3 triangles in an n-1 simplex
+
+void copy_point(const global Point p, global Point res)
+{
+	for (uint i = 0; i < RENDER_DIMENSION; ++i)
+	{
+		res[i] = p[i];
+	}
+}
+
+void emit_triangle(const global Point p0, const global Point p1, const global Point p2, global Triangle result)
+{
+	copy_point(p0, result[0]);
+	copy_point(p1, result[1]);
+	copy_point(p2, result[2]);
+}
+
+kernel void reduce_simplex(
+	const global Simplex* simplexes, 
+	global Triangle* result)
+{
+	const uint index = get_global_id(0) * TRIANGLES_PER_SIMPLEX;
+	uint res = index;
+	
+	for (uint i = 0; i < RENDER_DIMENSION - 2; ++i)
+	{
+		for (uint j = i + 1; j < REMDER_DIMENSION - 1; ++j)
+		{
+			for (uint k = j + 1; k < RENDER_DIMENSION; ++k)
+			{
+				emit_triangle(simplexes[index][i], simplexes[index][j], simplexes[index][k], result[res++]);
+			}
+		}	
+	}
+}
+
+)__CODE__";
