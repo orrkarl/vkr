a095ae9146f4570e57697d5282f9879d06528a76
orrkarl <karlor041@gmail.com>
2019-08-07 22:13:29 +0300

    refactored binning overflow test

--- test/inc/modules/binning/overflow.h
+++ test/inc/modules/binning/overflow.h
@@ -19,12 +19,16 @@ TEST(Binning, RasterizerOverflow)
 {
     cl_status err = CL_SUCCESS; 
 
-    const nr_uint dim = 4;
-    const nr_uint triangleCount = 400;
+    constexpr const nr_uint dim = 4;
+    constexpr const nr_uint triangleCount = 400;
+    constexpr const nr_uint workGroupCount = 2;
+    constexpr const ScreenDimension screenDim = {4, 10};
+    constexpr const BinQueueConfig config = {32, 32, 10};
 
-    const nr_uint workGroupCount = 2;
-    const ScreenDimension screenDim = {4, 10};
-    const BinQueueConfig config = {32, 32, 10};
+	constexpr nr_uint binCountX = compileTimeCeil(((nr_float)screenDim.width) / config.binWidth);
+	constexpr nr_uint binCountY = compileTimeCeil(((nr_float)screenDim.height) / config.binHeight);
+
+	using Queues = BinQueues<binCountX, binCountY, config.queueSize>;
 
     nr_bool isOverflowing = false;
 
@@ -40,33 +44,31 @@ TEST(Binning, RasterizerOverflow)
     auto code = mkBinningModule(dim, triangleCount, &err);
     ASSERT_SUCCESS(err);
 
-    auto testee = BinRasterizer(code, &err);
+    auto testee = BinRasterizerKernel(code, &err);
     ASSERT_SUCCESS(err);
     
-    auto d_triangles = Buffer::make<nr_float>(defaultContext, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, triangleCount * sizeof(Triangle<dim>) / sizeof(nr_float), (nr_float*) h_triangles, &err);
+    auto d_triangles = Buffer::make<Triangle<dim>>(defaultContext, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, triangleCount, h_triangles, &err);
     ASSERT_SUCCESS(err);
 
     auto d_overflow = Buffer::make<nr_uint>(defaultContext, CL_MEM_READ_WRITE, 1, &err);
     ASSERT_SUCCESS(err);
 
-    auto d_binQueues = Buffer::make<nr_uint>(defaultContext, CL_MEM_READ_WRITE, BinRasterizer::getTotalBinQueueCount(workGroupCount, screenDim, config), &err);
+    auto d_binQueues = Buffer::make<Queues>(defaultContext, CL_MEM_READ_WRITE, workGroupCount, &err);
     ASSERT_SUCCESS(err);
 
     auto d_batchIndex = Buffer::make<nr_uint>(defaultContext, CL_MEM_READ_WRITE, 1, &err);
     ASSERT_SUCCESS(err);
 
-    testee.binQueueConfig = config;
-    testee.dimension = screenDim;
-    testee.triangleData = d_triangles;
-    testee.triangleCount = triangleCount;
-    testee.hasOverflow = d_overflow;
-    testee.batchIndex = d_batchIndex;
+	testee.setExecutionRange(binCountX, binCountY, workGroupCount);
 
-    NDRange<2> global = { workGroupCount * screenDim.width, screenDim.height };
-    NDRange<2> local  = { screenDim.width, screenDim.height };
-
-    ASSERT_SUCCESS(testee.load());
-    ASSERT_SUCCESS(q.enqueueKernelCommand<2>(testee, global, local));
+	ASSERT_SUCCESS(testee.setBinQueueConfig(config));
+	ASSERT_SUCCESS(testee.setScreenDimension(screenDim));
+	ASSERT_SUCCESS(testee.setTriangleInputBuffer(d_triangles));
+	ASSERT_SUCCESS(testee.setTriangleCount(triangleCount));
+	ASSERT_SUCCESS(testee.setOvereflowNotifier(d_overflow));
+	ASSERT_SUCCESS(testee.setBinQueuesBuffer(d_binQueues));
+	ASSERT_SUCCESS(testee.setGlobalBatchIndex(d_batchIndex));
+	ASSERT_SUCCESS(q.enqueueDispatchCommand(testee));
     ASSERT_SUCCESS(q.enqueueBufferReadCommand(d_overflow, false, 1, &isOverflowing));
     ASSERT_SUCCESS(q.await());
     ASSERT_TRUE(isOverflowing);
