45326afe2b0f0c5d9b2fd1a28cdca9107a1a0ea4
orrkarl <karlor041@gmail.com>
2019-08-27 18:42:55 +0300

    documented vertex and simplex reduce

--- project/src/pipeline/VertexReducer.h
+++ project/src/pipeline/VertexReducer.h
@@ -1,3 +1,13 @@
+/**
+ * @file
+ * @author Orr Karl (karlor041@gmail.com)
+ * @brief implements the vertex reducer kernel
+ * @version 0.6.0
+ * @date 2019-08-27
+ * 
+ * @copyright Copyright (c) 2019
+ * 
+ */
 #pragma once
 
 #include "../predefs.h"
@@ -11,9 +21,31 @@ namespace nr
 namespace detail
 {
 
+/**
+ * @brief Accessing the first stage in the raster pipeline
+ * 
+ * The vertex reducer is the first stage in the raster pipeline. It's job is to reduce each vertex dimensions - 
+ * take the homogenous N dimensional input and transform it into a 2D position with some extra data (such as 'depth'), to be used in 
+ * the next pipeline operations.
+ * As of right now, this stage runs repeating perspective projections on each dimension.
+ * @par
+ * This stage takes 4 inputs: the vertex input buffer, the perspective near and far planes and an output target buffer.
+ * It executes naively - each vertex gets it's own work item to process.
+ * Each simplex will be copied to the target buffer and it's vertecies will be reduced; simplex order in the input buffer is preserved.
+ * @par
+ * The vertecies are reduced to a D3D style 'NDC' form - the X,Y coordinates are between -1 and 1, while every other coordinate is between 0 and 1.
+ * 
+ * @note As of right now, no clipping is done at all - but this will change soon
+ */
 class VertexReducer : public StandardDispatch<1, Buffer, Buffer, Buffer, Buffer>
 {
 public:
+	/**
+	 * @brief Find the vertex reduce kernel in a module and creates accessor to it
+	 * 
+	 * @param module kernel containing module
+	 * @param[out] err internal OpenCL call status
+	 */
 	VertexReducer(const Module& module, cl_status* err = nullptr)
 		: StandardDispatch(module, VERTEX_REDUCE_KERNEL_NAME, err)
 	{
@@ -24,26 +56,63 @@ public:
 	{
 	}
 
+	/**
+	 * @brief Set the kernel expected input buffer
+	 * 
+	 * The buffer is expected to contain plain simplexes: N points of homogenous N dimension (N + 1 coordinates)
+	 * @param in data buffer
+	 * @return cl_status internal OpenCL call status
+	 */
 	cl_status setSimplexInputBuffer(const Buffer& in)
 	{
 		return setArg<INPUT_BUFFER>(in);
 	}
 
+	/**
+	 * @brief Set the perspective near plane
+	 * 
+	 * The buffer is expected to contain N elements (one float each).
+	 * Each element specifices the lower bound of the repeating perspective projections at it's coordinate
+	 * @param near perspective near plane
+	 * @return cl_status internal OpenCL call status
+	 */
 	cl_status setNearPlaneBuffer(const Buffer& near)
 	{
 		return setArg<NEAR_PLANE_BUFFER>(near);
 	}
 
+	/**
+	 * @brief Set the perspective far plane
+	 * 
+	 * The buffer is expected to contain N elements (one float each).
+	 * Each element specifices the upper bound of the repeating perspective projections at it's coordinate
+	 * @param far perspective far plane
+	 * @return cl_status internal OpenCL call status
+	 */
 	cl_status setFarPlaneBuffer(const Buffer& far)
 	{
 		return setArg<FAR_PLANE_BUFFER>(far);
 	}
 
+	/**
+	 * @brief Set the simplex output buffer
+	 * 
+	 * Sets the target buffer for this stage
+	 * @param out simplex target buffer
+	 * @return cl_status internal OpenCL call status
+	 */
 	cl_status setSimplexOutputBuffer(const Buffer& out)
 	{
 		return setArg<OUTPUT_BUFFER>(out);
 	}
 
+	/**
+	 * @brief Set the execution range
+	 * 
+	 * Since there is a one to one mapping between vertecies and work items, this just creates a single line of work items
+	 * , with length the same as the vertex count
+	 * @param vertexCount vertex count in batch
+	 */
 	void setExecutionRange(const nr_uint vertexCount)
 	{
 		range.global.x = vertexCount;
