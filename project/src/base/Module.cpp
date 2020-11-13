62f03c47185d5f64d9a0799404cf3b084c837bf6
orrkarl <karlor041@gmail.com>
2020-11-14 00:17:56 +0200

    implemented Module

--- project/src/base/Module.cpp
+++ project/src/base/Module.cpp
@@ -6,168 +6,98 @@
 #include <sstream>
 #include <string>
 
-namespace nr
-{
+namespace nr::base {
 
-Module::Option::Option(const string& value)
-    : value(value)
-{
-}
-
-const string& Module::Option::getOption() const 
-{ 
-    return value; 
-}
-
-Module::Macro::Macro(const string name)
-    : Module::Option("-D " + name)
+BuildFailedException::BuildFailedException(Status status)
+    : CLApiException(status, "could not build program")
 {
 }
 
-Module::Macro::Macro(const string name, const string value)
-    : Module::Option("-D " + name + "=" + value)
+cl_program createProgram(cl_context context, const std::vector<std::string>& sources)
 {
-}
+    Status status = CL_SUCCESS;
 
-Module::CLVersion::CLVersion(const string& version)
-    : Module::Option("-cl-std=CL" + version)
-{
-}
-
-Module::Module()
-    : Wrapped()
-{
-}
-
-Module::Module(const Context& context, const string& code, cl_status& err)
-    : Wrapped()
-{
-    size_t s = code.size();
-    auto code_ptr = code.c_str();
-    object = clCreateProgramWithSource(context, 1, &code_ptr, &s, &err);
-}
-
-Module::Module(const Context& context, const Sources& codes, cl_status& err)
-    : Wrapped() 
-{
-    std::vector<const nr_char*> codesRaw(codes.size());
-    std::transform(codes.cbegin(), codes.cend(), codesRaw.begin(), std::mem_fn(&string::c_str));
-    object = clCreateProgramWithSource(context, static_cast<nr_uint>(codes.size()), &codesRaw[0], nullptr, &err);
-}
-
-Module::Module(const cl_program& module, const nr_bool retain, cl_status& status)
-	: Wrapped(module, retain, status)
-{
-}
-
-
-Module::Module(const Module& other)
-    : Wrapped(other)
-{
-}
-
-Module::Module(Module&& other)
-    : Wrapped(other)
-{
-}
-
-Module& Module::operator=(const Module& other)
-{
-    Wrapped::operator=(other);
-    return *this;
-}
-
-Module& Module::operator=(Module&& other)
-{
-    Wrapped::operator=(other);
-    return *this;
-}
-
-Module::operator cl_program() const
-{
-    return get();
-}
-
-cl_program Module::get() const
-{
-	return object;
-}
-
-cl_status Module::build(const Device& device, const Options& options)
-{
-    return clBuildProgram(
-        object, 
-        1, &device.get(), 
-        Module::finalizeOptions(options).c_str(), 
-        nullptr, 
-        nullptr);
-}
-
-cl_status Module::build(const Devices& devices, const Options& options)
-{
-    return clBuildProgram(
-        object, 
-		static_cast<nr_uint>(devices.size()), (const cl_device_id*) &devices[0],
-        Module::finalizeOptions(options).c_str(), 
-        nullptr, 
-        nullptr);
-}
-
-Kernel Module::createKernel(const string& name, cl_status& err) const
-{
-    return createKernel(name.c_str(), err);
-}
-
-Kernel Module::createKernel(const char* name, cl_status& err) const
-{
-	return Kernel(*this, name, err);
-}
-
-string Module::finalizeOptions(const Options& options)
-{
-    string ret = "";
-    for (auto it = options.cbegin(); it != options.cend(); ++it)
-    {
-        ret += " " + it->getOption();
+    std::vector<const char*> sourcesRaw(sources.size());
+    std::transform(sources.cbegin(), sources.cend(), sourcesRaw.begin(), std::mem_fn(&std::string::c_str));
+    auto ret = clCreateProgramWithSource(
+        context, static_cast<U32>(sources.size()), sourcesRaw.data(), nullptr, &status);
+    if (status != CL_SUCCESS) {
+        throw ModuleCreateException(status);
     }
 
-	return ret;    
+    return ret;
 }
 
-string Module::getBuildLog(Device device, cl_status& err) const
+Module::Module(Context& context, const std::string& code)
+    : m_object(createProgram(context.rawHandle(), { code }))
 {
-	nr_size len = 0;
-	err = clGetProgramBuildInfo(object, device, CL_PROGRAM_BUILD_LOG, len, nullptr, &len);
-	std::unique_ptr<nr_char[]> str = std::make_unique<nr_char[]>(len);
-	err = clGetProgramBuildInfo(object, device, CL_PROGRAM_BUILD_LOG, len, str.get(), nullptr);
-	return string(str.get());
 }
 
-string Module::getKernelNames(cl_status& err) const
+Module::Module(Context& context, const Sources& codes)
+    : m_object(createProgram(context.rawHandle(), codes))
 {
-	nr_char* rawNames;
-	nr_uint count;
-
-	err = clGetProgramInfo(object, CL_PROGRAM_KERNEL_NAMES, sizeof(nr_char*), &rawNames, nullptr);
-	if (error::isFailure(err))
-	{
-		return "";
-	}
-
-	err = clGetProgramInfo(object, CL_PROGRAM_NUM_KERNELS, sizeof(nr_uint), &count, nullptr);
-	if (error::isFailure(err))
-	{
-		return "";
-	}
-
-	return string(rawNames);
 }
 
-const Module::Macro           Module::DEBUG               = Module::Macro("_DEBUG");
-const Module::CLVersion       Module::CL_VERSION_22       = Module::CLVersion("2.2");
-const Module::CLVersion       Module::CL_VERSION_21       = Module::CLVersion("2.1");
-const Module::CLVersion       Module::CL_VERSION_20       = Module::CLVersion("2.0");
-const Module::CLVersion       Module::CL_VERSION_12       = Module::CLVersion("1.2");
-const Module::CLVersion       Module::CL_VERSION_11       = Module::CLVersion("1.1");
+cl_program Module::rawHandle() { return m_object; }
+
+void Module::build(DeviceView device, const char* options)
+{
+    Devices devices { device };
+    build(devices, options);
+}
+
+void Module::build(Devices& devices, const char* options)
+{
+    if (devices.size() > std::numeric_limits<U32>::max()) {
+        throw BuildFailedException(CL_INVALID_VALUE);
+    }
+
+    std::vector<cl_device_id> rawDevices(devices.size());
+    std::transform(devices.begin(), devices.end(), rawDevices.begin(), std::mem_fn(&DeviceView::rawHandle));
+
+    auto status = clBuildProgram(
+        m_object, static_cast<U32>(devices.size()), rawDevices.data(), options, nullptr, nullptr);
+    if (status != CL_SUCCESS) {
+        throw BuildFailedException(status);
+    }
+}
+
+std::string Module::getBuildLog(DeviceView device) const
+{
+    size_t len = 0;
+
+    auto status = clGetProgramBuildInfo(m_object, device.rawHandle(), CL_PROGRAM_BUILD_LOG, 0, nullptr, &len);
+    if (status != CL_SUCCESS) {
+        throw CLApiException(status, "could not query build log length");
+    }
+
+    auto str = std::make_unique<char[]>(len);
+    status
+        = clGetProgramBuildInfo(m_object, device.rawHandle(), CL_PROGRAM_BUILD_LOG, len, str.get(), nullptr);
+    if (status != CL_SUCCESS) {
+        throw CLApiException(status, "could not query build log");
+    }
+
+    return std::string(str.get(), len);
+}
+
+std::vector<std::string> Module::getKernelNames() const
+{
+    char* rawNames = nullptr;
+    auto status = clGetProgramInfo(m_object, CL_PROGRAM_KERNEL_NAMES, sizeof(rawNames), &rawNames, nullptr);
+    if (status != CL_SUCCESS) {
+        throw CLApiException(status, "could not query kernel names");
+    }
+
+    std::vector<std::string> ret;
+
+    std::istringstream splittingNames(rawNames);
+    std::string kernelName;
+    while (std::getline(splittingNames, kernelName, ';')) {
+        ret.push_back(kernelName);
+    }
+
+    return ret;
+}
 
 }
