d930592e66247353d039b74db6235af9fabb5660
orrkarl <karlor041@gmail.com>
2020-11-13 20:17:18 +0200

    implemented Event

--- project/src/base/Event.cpp
+++ project/src/base/Event.cpp
@@ -1,58 +1,58 @@
 #include "Event.h"
 
-namespace nr
-{
+namespace nr::base {
 
-Event::Event()
-    : Wrapped()
+EventWaitException::EventWaitException(Status status)
+    : CLApiException(status, "could not wait for event")
 {
 }
 
-Event::Event(const cl_event& event, const nr_bool retain, cl_status& status)
-    : Wrapped(event, retain, status)
+void EventView::await(const std::vector<EventView>& events)
+{
+    if (events.size() > std::numeric_limits<U32>::max()) {
+        throw EventWaitException(CL_INVALID_VALUE);
+    }
+
+    std::vector<cl_event> rawEvents(events.size());
+    std::transform(
+        events.cbegin(), events.cend(), rawEvents.begin(), [](EventView event) { return event.m_event; });
+
+    auto status = clWaitForEvents(static_cast<U32>(events.size()), rawEvents.data());
+    if (status != CL_SUCCESS) {
+        throw EventWaitException(status);
+    }
+}
+
+EventView::EventView(cl_event rawEvent)
+    : m_event(rawEvent)
 {
 }
 
-Event::Event(const Context& context, cl_status& err)
-    : Wrapped(clCreateUserEvent(context, &err))
+void EventView::await() const
+{
+    auto status = clWaitForEvents(1, &m_event);
+    if (status != CL_SUCCESS) {
+        throw EventWaitException(status);
+    }
+}
+
+cl_event createUserEvent(Context& context)
+{
+    Status status = CL_SUCCESS;
+
+    auto ret = clCreateUserEvent(context.rawHandle(), &status);
+    if (status != CL_SUCCESS) {
+        throw EventCreateException(status);
+    }
+
+    return ret;
+}
+
+Event::Event(Context& context)
+    : m_object(createUserEvent(context))
 {
 }
 
-Event::Event(const Event& other)
-    : Wrapped(other)
-{
-}
-
-Event::Event(Event&& other)
-    : Wrapped(other)
-{
-}
-
-Event& Event::operator=(const Event& other)
-{
-    Wrapped::operator=(other);
-    return *this;
-}
-
-Event& Event::operator=(Event&& other)
-{
-    Wrapped::operator=(other);
-    return *this;
-}
-
-Event::operator cl_event() const
-{
-    return object;
-}
-
-cl_status Event::await() const
-{
-    return clWaitForEvents(1, &object);
-}
-
-cl_status Event::await(const std::vector<Event>& events)
-{
-    return clWaitForEvents(cl_uint(events.size()), (const cl_event*) &events.front());
-}
+EventView Event::view() { return EventView(m_object); }
 
 }
--- project/src/base/Event.h
+++ project/src/base/Event.h
@@ -1,13 +1,3 @@
-/**
- * @file
- * @author Orr Karl (karlor041@gmail.com)
- * @brief provides a C++ style wrapper for OpenCL cl_event
- * @version 0.6.0
- * @date 2019-08-26
- * 
- * @copyright Copyright (c) 2019
- * 
- */
 #pragma once
 
 #include "../predefs.h"
@@ -18,90 +8,63 @@
 #include "Context.h"
 #include "Wrapper.h"
 
-namespace nr
-{
+namespace nr::base {
+
+class EventWaitException : public CLApiException {
+public:
+    explicit EventWaitException(Status status);
+};
+
+CL_TYPE_CREATE_EXCEPTION(Event);
 
 /**
- * @brief Wrapper class for OpenCL cl_event - used for synchronization between commands, the device and the host
- * 
+ * @brief Wrapper class for OpenCL cl_event - used for synchronization between commands, the device and the
+ * host
  */
-class NRAPI Event : public Wrapper<cl_event>
-{
+class EventView {
 public:
     /**
-     * @brief Construct a null event
-     * 
+     * @brief Blocks until all events in list reached CL_COMPLETE
+     *
+     * @note although it accepts a vector, passing more than MAX_UINT elements will throw
+     *
+     * @param events event list to wait
+     * @return cl_status internal OpenCL call status
      */
-    Event();
+    static void await(const std::vector<EventView>& events);
 
-    /**
-	 * @brief Convertes a raw OpenCL event to the C++ wrapper
-	 * 
-	 * @param event object to own
-	 * @param retain should the reference count for the object be incremented
-	 */
-    Event(const cl_event& event, const nr_bool retain, cl_status& status);
-
-    /**
-     * @brief Creates a new event, bound to a ceratin context
-     * 
-     * @note wraps clCreateUserEvent
-     * @param context parent context for this event
-     * @param[out] err internal OpenCL call status
-     */
-    Event(const Context& context, cl_status& err);
-
-    Event(const Event& other);
-
-    Event(Event&& other);
-
-    Event& operator=(const Event& other);
-
-    Event& operator=(Event&& other);
-
-    /**
-     * @brief provides access to the underlying OpenCL event
-     * 
-     * @return cl_event underlying OpenCL command queue
-     */
-    operator cl_event() const;
+    explicit EventView(cl_event rawEvent);
 
     /**
      * @brief Blocks until this event reaches CL_COMPLETE
      * @note wraps clWaitForEvents
      * @return cl_status internal OpenCL call status
      */
-    cl_status await() const;
+    void await() const;
 
-    /**
-     * @brief Registeres a callback to this event
-     * 
-     * The callback will be called every time the event reaches a certain stage
-     * @tparam T user data type
-     * @param listenedStatus the stage this callback is registered to
-     * @param callback the user callback
-     * @param data user data
-     * @return cl_status internal OpenCL call status
-     */
-    template<typename T>
-    cl_status registerCallback(
-        cl_int listenedStatus, 
-        const std::function<void(Event, cl_int, T*)> callback, T* data)
-    {
-        return clSetEventCallback(
-            object, listenedStatus, 
-            static_cast<void(CL_CALLBACK *)(cl_event, cl_int, void*)>(
-                [callback](cl_event event, cl_int status, void* data){ callback(Event(event, true), status, (T*) data); }), 
-            data);
-    }
+private:
+    cl_event m_event;
+};
 
+class Event {
+public:
     /**
-     * @brief Blocks until all events in list reached CL_COMPLETE
-     * 
-     * @param events event list to wait
-     * @return cl_status internal OpenCL call status 
+     * @brief Creates a new event, bound to a certain context
+     *
+     * @note wraps clCreateUserEvent
+     * @param context parent context for this event
      */
-    static cl_status await(const std::vector<Event>& events);
+    explicit Event(Context& context);
+
+    EventView view();
+
+private:
+    struct EventTraits {
+        static constexpr auto release = clReleaseEvent;
+        using Type = cl_event;
+    };
+
+    UniqueWrapper<EventTraits> m_object;
 };
 
 }
