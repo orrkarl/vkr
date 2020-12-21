e9a976d90e1a364c6820a15278a85372f32f7c02
orrkarl <karlor041@gmail.com>
2020-12-22 00:48:43 +0200

    purged everything outside of project skeleton

---
+++ src/Example.cpp
@@ -0,0 +1,9 @@
+#include <vulkan/vulkan.hpp>
+
+namespace vkr {
+
+uint32_t example() {
+    return 1;
+}
+
+}
---
+++ test/src/example/TestFail.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+#include <catch2/catch.hpp>
+
+TEST_CASE("Sanity", "[sanity]") {
+    REQUIRE(false);
+}
