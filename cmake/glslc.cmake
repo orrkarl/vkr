1b44e68eae52113bfa784e040a2dd2825c1aa660
orrkarl <karlor041@gmail.com>
2020-12-21 20:42:16 +0200

    adding glslc

---
+++ cmake/glslc.cmake
@@ -0,0 +1,10 @@
+IF (NOT ${Vulkan_FOUND})
+    FIND_PACKAGE(Vulkan REQUIRED)
+ENDIF
+
+function(add_shader TARGET_NAME SHADER DESTINATION)
+    add_custom_target(${TARGET_NAME} ALL
+        COMMAND ${Vulkan_GLSLC_EXECUTABLE} -o ${CMAKE_CURRENT_BINARY_DIR}/${DESTINATION} ${CMAKE_CURRENT_SOURCE_DIR}/${SHADER}
+        VERBATIM
+    )
+endfunction()
