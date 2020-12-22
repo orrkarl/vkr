b658552ac12dd28cf5a61fb2c08a5b86cf81e94a
orrkarl <karlor041@gmail.com>
2020-12-22 02:09:16 +0200

    test dir skeleton now reflects using vkr-backend

--- test/src/example/TestFail.cpp
+++ test/src/example/TestFail.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 
 #include <catch2/catch.hpp>
+#include <gpu/AllKernels.h>
 
 TEST_CASE("Sanity", "[sanity]") {
-    REQUIRE(false);
+    auto simpleModuleInfo = vkr::gpu::describeSimple();
+    REQUIRE(simpleModuleInfo.pCode[0] == 0xAABBCCDD);
 }
