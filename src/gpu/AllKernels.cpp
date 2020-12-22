89d1d35d0609307f6c7d55e6ad298f12316c9f7d
orrkarl <karlor041@gmail.com>
2020-12-22 20:18:34 +0200

    renamed embedded size var

--- src/gpu/AllKernels.cpp
+++ src/gpu/AllKernels.cpp
@@ -4,7 +4,7 @@ namespace vkr {
 namespace gpu {
 
 vk::ShaderModuleCreateInfo describeSimple() {
-    return vk::ShaderModuleCreateInfo(vk::ShaderModuleCreateFlags(), device_side::Simple_comp_size * sizeof(uint32_t), device_side::Simple_comp);
+    return vk::ShaderModuleCreateInfo(vk::ShaderModuleCreateFlags(), device_side::Simple_comp_count * sizeof(uint32_t), device_side::Simple_comp);
 }
 
 
