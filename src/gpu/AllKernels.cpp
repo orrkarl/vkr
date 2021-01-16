565de6cbbe5c21dc94593d26845813f4295e7039
orrkarl <karlor041@gmail.com>
2021-01-16 21:00:31 +0200

    moving to a C-like API

--- src/gpu/AllKernels.cpp
+++ src/gpu/AllKernels.cpp
@@ -3,10 +3,10 @@
 namespace vkr {
 namespace gpu {
 
-vk::ShaderModuleCreateInfo describeTriangleSetup() {
-    return vk::ShaderModuleCreateInfo(vk::ShaderModuleCreateFlags(),
+VkShaderModuleCreateInfo describeTriangleSetup() {
+    return VkShaderModuleCreateInfo { VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO, nullptr, 0,
                                       device_side::TriangleSetup_comp_count * sizeof(uint32_t),
-                                      device_side::TriangleSetup_comp);
+                                      device_side::TriangleSetup_comp };
 }
 
 } // gpu
