09fdc242af1fae44bab954e34a24da7d991a2dc8
orrkarl <karlor041@gmail.com>
2021-01-17 23:12:17 +0200

    better choosing AllKernels.h interface

--- src/gpu/AllKernels.h
+++ src/gpu/AllKernels.h
@@ -9,7 +9,14 @@
 namespace vkr {
 namespace gpu {
 
-VkShaderModuleCreateInfo describeTriangleSetup();
+namespace triangle_setup {
+
+VkShaderModule createModule(VkResult& status, VkDevice dev, const VkAllocationCallbacks* allocator = nullptr);
+VkDescriptorSetLayout createLayout(VkResult& status, VkDevice dev,
+                                   const VkAllocationCallbacks* allocator = nullptr);
+VkPushConstantRange describePushConstants();
+
+} // triangle_setup
 
 } // gpu
 } // vkr
