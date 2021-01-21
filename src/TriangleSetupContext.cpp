379d661f821bd0a812cc53ab0cf736a0a8be956a
orrkarl <karlor041@gmail.com>
2021-01-21 19:33:44 +0200

    removed kernels interface

--- src/TriangleSetupContext.cpp
+++
@@ -1,7 +0,0 @@
-#include "TriangleSetupContext.h"
-
-#include "gpu/AllKernels.h"
-#include "gpu/shared/compatibility.h"
-
-TriangleSetupContext::TriangleSetupContext() {
-}
--- src/TriangleSetupContext.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-
-class TriangleSetupContext
-{
-public:
-    TriangleSetupContext();
-};
-
