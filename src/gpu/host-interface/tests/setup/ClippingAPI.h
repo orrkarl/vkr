5b20d69206ae6078556da46bfa5ba7f315b77b83
orrkarl <karlor041@gmail.com>
2021-02-06 12:52:20 +0200

    reformatted ClippingAPI.h

--- src/gpu/host-interface/tests/setup/ClippingAPI.h
+++ src/gpu/host-interface/tests/setup/ClippingAPI.h
@@ -20,25 +20,28 @@ struct ClippingLayout {
     static const std::array<uint32_t, 1> SETS;
     static const std::array<VkPushConstantRange, 1> PUSH_ARGS;
 
-    static constexpr std::pair<uint32_t, uint32_t> VERTECIES_POS = {0, 0};
-    static constexpr std::pair<uint32_t, uint32_t> CLIPPED_VERTECIES_POS = {0, 1};
-    static constexpr std::pair<uint32_t, uint32_t> CLIPPED_VERTEX_COUNTS = {0, 2};
+    static constexpr std::pair<uint32_t, uint32_t> VERTECIES_POS = { 0, 0 };
+    static constexpr std::pair<uint32_t, uint32_t> CLIPPED_VERTECIES_POS = { 0, 1 };
+    static constexpr std::pair<uint32_t, uint32_t> CLIPPED_VERTEX_COUNTS = { 0, 2 };
 
     static constexpr uint32_t TRIANGLE_COUNT_OFFSET = 0;
     static constexpr uint32_t TRIANGLE_COUNT_SIZE = sizeof(uint32_t);
 };
 
-}
+} // namespace detail
 
 class ClippingAPI : public DeviceModuleAPI<detail::ClippingLayout> {
 public:
-    VkWriteDescriptorSet describeVerteciesUpdate(VkDescriptorSet args, const VkDescriptorBufferInfo& vertecies);
-    VkWriteDescriptorSet describeClippedVerteciesUpdate(VkDescriptorSet args, const VkDescriptorBufferInfo& clippedVertecies);
-    VkWriteDescriptorSet describeClippedVertexCountsUpdate(VkDescriptorSet args, const VkDescriptorBufferInfo& clippedVertexCounts);
+    VkWriteDescriptorSet describeVerteciesUpdate(VkDescriptorSet args,
+                                                 const VkDescriptorBufferInfo& vertecies);
+    VkWriteDescriptorSet describeClippedVerteciesUpdate(VkDescriptorSet args,
+                                                        const VkDescriptorBufferInfo& clippedVertecies);
+    VkWriteDescriptorSet describeClippedVertexCountsUpdate(VkDescriptorSet args,
+                                                           const VkDescriptorBufferInfo& clippedVertexCounts);
 
     void cmdUpdateTriangleCount(VkCommandBuffer cmdBuffer, u32 triangleCount);
 };
 
-}
-}
-}
+} // namespace tests
+} // namespace gpu
+} // namespace vkr
