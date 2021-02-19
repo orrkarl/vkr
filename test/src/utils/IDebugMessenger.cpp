3b5e1b6614365fcd892d6e87f40ae6ff7e127d79
orrkarl <karlor041@gmail.com>
2021-02-19 18:49:34 +0200

    moved severities and types into IDebugMessenger ctor

--- test/src/utils/IDebugMessenger.cpp
+++ test/src/utils/IDebugMessenger.cpp
@@ -1,35 +1,56 @@
 #include "IDebugMessenger.h"
 
+#include <sstream>
+#include <type_traits>
+
 namespace utils {
 
 vk::Bool32 IDebugMessenger::debugCallback(VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
                                           VkDebugUtilsMessageTypeFlagsEXT messageType,
                                           const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData,
                                           void* pUserData) {
-    reinterpret_cast<IDebugMessenger*>(pUserData)->debugCallback(messageSeverity, messageType, pCallbackData);
+    reinterpret_cast<IDebugMessenger*>(pUserData)->debugCallback(
+        vk::DebugUtilsMessageSeverityFlagsEXT(messageSeverity),
+        vk::DebugUtilsMessageTypeFlagsEXT(messageType),
+        reinterpret_cast<const vk::DebugUtilsMessengerCallbackDataEXT*>(pCallbackData));
     return VK_FALSE;
 }
 
-vk::DebugUtilsMessengerCreateInfoEXT IDebugMessenger::describeMessenger(
-    vk::DebugUtilsMessageSeverityFlagsEXT allowedSeverity, vk::DebugUtilsMessageTypeFlagsEXT allowedTypes) {
-    return vk::DebugUtilsMessengerCreateInfoEXT(vk::DebugUtilsMessengerCreateFlagsEXT(),
-                                                allowedSeverity,
-                                                allowedTypes,
-                                                &IDebugMessenger::debugCallback,
-                                                this);
+IDebugMessenger::IDebugMessenger(vk::DebugUtilsMessageSeverityFlagsEXT severities,
+                                 vk::DebugUtilsMessageTypeFlagsEXT types)
+    : m_severities(severities), m_types(types) {
 }
 
-DebugMessengerRegisterGuard::DebugMessengerRegisterGuard(
-    vk::Instance inst,
-    vk::DebugUtilsMessageSeverityFlagsEXT allowedSeverities,
-    vk::DebugUtilsMessageTypeFlagsEXT allowedTypes,
-    IDebugMessenger& messenger,
-    vk::Optional<const vk::AllocationCallbacks> allocator)
-    : m_messengerHandle(
-        inst.createDebugUtilsMessengerEXTUnique(messenger.describeMessenger(allowedSeverities, allowedTypes),
-                                                allocator,
-                                                vk::DispatchLoaderDynamic(inst, vkGetInstanceProcAddr)))
-    , m_userMessengerObj(messenger) {
+vk::DebugUtilsMessengerCreateInfoEXT IDebugMessenger::describeMessenger() {
+    return vk::DebugUtilsMessengerCreateInfoEXT(
+        vk::DebugUtilsMessengerCreateFlagsEXT(), m_severities, m_types, &IDebugMessenger::debugCallback, this);
+}
+
+std::string IDebugMessenger::stringifyDebugMessage(vk::DebugUtilsMessageSeverityFlagsEXT severity,
+                                                   vk::DebugUtilsMessageTypeFlagsEXT type,
+                                                   const vk::DebugUtilsMessengerCallbackDataEXT* callbackData) {
+    std::stringstream ss;
+    ss << "==========================================================================" << std::endl;
+    ss << "DEBUG Callback Message: " << callbackData->pMessageIdName << " (" << callbackData->messageIdNumber << ")"
+       << std::endl;
+    ss << "\tSEVERITIES = " << vk::to_string(severity) << std::endl;
+    ss << "\tTYPES = " << vk::to_string(type) << std::endl;
+    ss << callbackData->pMessage << std::endl;
+    ss << "==========================================================================" << std::endl;
+    return ss.str();
+}
+
+vk::DebugUtilsMessageSeverityFlagsEXT IDebugMessenger::minSeverityToBitmap(
+    vk::DebugUtilsMessageSeverityFlagBitsEXT min) {
+    auto arithmeticSeverity = static_cast<std::underlying_type_t<vk::DebugUtilsMessageSeverityFlagBitsEXT>>(min);
+    return ALL_SEVERITIES & ~(vk::DebugUtilsMessageSeverityFlagsEXT(arithmeticSeverity - 1));
+}
+
+DebugMessengerRegisterGuard::DebugMessengerRegisterGuard(vk::Instance inst,
+                                                         const vk::DispatchLoaderDynamic& dynamics,
+                                                         IDebugMessenger& messenger,
+                                                         vk::Optional<const vk::AllocationCallbacks> allocator)
+    : m_messengerHandle(inst.createDebugUtilsMessengerEXTUnique(messenger.describeMessenger(), allocator, dynamics)) {
 }
 
 } // namespace utils
--- test/src/utils/IDebugMessenger.h
+++ test/src/utils/IDebugMessenger.h
@@ -6,45 +6,56 @@ namespace utils {
 
 class IDebugMessenger {
 public:
+    static constexpr vk::DebugUtilsMessageSeverityFlagsEXT ALL_SEVERITIES = vk::DebugUtilsMessageSeverityFlagsEXT(
+        vk::FlagTraits<vk::DebugUtilsMessageSeverityFlagBitsEXT>::allFlags);
+
+    static constexpr vk::DebugUtilsMessageTypeFlagsEXT ALL_TYPES = vk::DebugUtilsMessageTypeFlagsEXT(
+        vk::FlagTraits<vk::DebugUtilsMessageTypeFlagBitsEXT>::allFlags);
+
+    IDebugMessenger(vk::DebugUtilsMessageSeverityFlagsEXT severities = ALL_SEVERITIES,
+                    vk::DebugUtilsMessageTypeFlagsEXT types = ALL_TYPES);
+
     virtual ~IDebugMessenger() = default;
 
-    vk::DebugUtilsMessengerCreateInfoEXT describeMessenger(
-        vk::DebugUtilsMessageSeverityFlagsEXT allowedSeverity = vk::DebugUtilsMessageSeverityFlagsEXT(
-            vk::FlagTraits<vk::DebugUtilsMessageSeverityFlagBitsEXT>::allFlags),
-        vk::DebugUtilsMessageTypeFlagsEXT allowedTypes = vk::DebugUtilsMessageTypeFlagsEXT(
-            vk::FlagTraits<vk::DebugUtilsMessageTypeFlagBitsEXT>::allFlags));
+    vk::DebugUtilsMessengerCreateInfoEXT describeMessenger();
 
 protected:
-    virtual void debugCallback(VkDebugUtilsMessageSeverityFlagsEXT severity,
-                               VkDebugUtilsMessageTypeFlagsEXT type,
-                               const VkDebugUtilsMessengerCallbackDataEXT* callbackData)
+    static std::string stringifyDebugMessage(vk::DebugUtilsMessageSeverityFlagsEXT severity,
+                                             vk::DebugUtilsMessageTypeFlagsEXT type,
+                                             const vk::DebugUtilsMessengerCallbackDataEXT* callbackData);
+
+    static vk::DebugUtilsMessageSeverityFlagsEXT minSeverityToBitmap(vk::DebugUtilsMessageSeverityFlagBitsEXT min);
+
+    virtual void debugCallback(vk::DebugUtilsMessageSeverityFlagsEXT severity,
+                               vk::DebugUtilsMessageTypeFlagsEXT type,
+                               const vk::DebugUtilsMessengerCallbackDataEXT* callbackData)
         = 0;
 
 private:
-    static VKAPI_ATTR vk::Bool32 VKAPI_CALL
-    debugCallback(VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
-                  VkDebugUtilsMessageTypeFlagsEXT messageType,
-                  const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData,
-                  void* pUserData);
+    static VKAPI_ATTR vk::Bool32 VKAPI_CALL debugCallback(VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
+                                                          VkDebugUtilsMessageTypeFlagsEXT messageType,
+                                                          const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData,
+                                                          void* pUserData);
 
     // since a pointer to the messenger is passed to vulkan, moving/copying is invalid
     IDebugMessenger(const IDebugMessenger&) = delete;
     IDebugMessenger& operator=(const IDebugMessenger&) = delete;
     IDebugMessenger(IDebugMessenger&&) = delete;
     IDebugMessenger& operator=(IDebugMessenger&&) = delete;
+
+    vk::DebugUtilsMessageSeverityFlagsEXT m_severities;
+    vk::DebugUtilsMessageTypeFlagsEXT m_types;
 };
 
 class DebugMessengerRegisterGuard {
 public:
     DebugMessengerRegisterGuard(vk::Instance inst,
-                                vk::DebugUtilsMessageSeverityFlagsEXT allowedSeverities,
-                                vk::DebugUtilsMessageTypeFlagsEXT allowedTypes,
+                                const vk::DispatchLoaderDynamic& dynamics,
                                 IDebugMessenger& messenger,
                                 vk::Optional<const vk::AllocationCallbacks> allocator = nullptr);
 
 private:
     vk::UniqueHandle<vk::DebugUtilsMessengerEXT, vk::DispatchLoaderDynamic> m_messengerHandle;
-    IDebugMessenger& m_userMessengerObj;
 };
 
 } // namespace utils
